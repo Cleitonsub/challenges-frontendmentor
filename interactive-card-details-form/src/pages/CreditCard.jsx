473a363b51102fdc08a49f42488217c688dfd63b
Cleitonsub <cleitonrmacedo@gmail.com>
2023-10-25 17:09:43 -0300

    feat: Adicionado na pagina /creditCard componentes ImgCreditCard e FormCreditCard

--- interactive-card-details-form/src/pages/CreditCard.jsx
+++ interactive-card-details-form/src/pages/CreditCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import bgMainDesktop from '../images/bg_main_desktop.png';
+import bgMainMobile from '../images/bg_main_mobile.png';
+import ImgCreditCard from '../components/ImgCreditCard';
+import FormCreditCard from '../components/FormCreditCard';
+
 
 export default function CreditCard() {
     return (
         <>
             <img src={ bgMainDesktop } alt="bg_main_desktop" />
+            <img src={ bgMainMobile } alt="bg_main_mobile" />
+            <div className="container">
+                <ImgCreditCard />
+                <FormCreditCard />
+            </div>
         </>
     )
 }
